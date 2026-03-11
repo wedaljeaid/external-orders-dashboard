b471f470a57303e2d072d78934216a5bcb0a2748
wedaljeaid <wedaljeaid@gmail.com>
2026-03-12 00:50:07 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
-﻿window.DASHBOARD_CONFIG = {
+window.DASHBOARD_CONFIG = {
   googleSheetId: "1LM7ZYKm8PNG8kRKx5i5rWTGdY5csSlhO",
   googleSheetGid: "0",
-  publishedCsvUrl: "",
+  publishedCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSNwzGv5TkL12SM7q-EodQPyaTBGRdGZD8LJLQCmXsquE_pr2mSQhPvrL3OvbvvqQ/pub?output=csv",
   dataEndpoint: "",
   sourceLabel: "Google Sheets",
   autoRefreshMinutes: 15,
   fallbackDataUrl: "./data/external_orders.json"
 };
+
